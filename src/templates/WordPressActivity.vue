750585d09bdcbe118415a728cb4cb02780a225e6
Egor <160damage@gmail.com>
2020-04-21 00:10:49 +0300

    add grids

--- src/templates/WordPressActivity.vue
+++ src/templates/WordPressActivity.vue
@@ -132,7 +132,84 @@
                     </div>
                 </section>
             </div>
+            <div class="periods">
+                <article class="period" v-for="(period, index) of this.$page.wordPressActivity.acf.periods"
+                         :key="index">
+                    <div class="period__main"
+                         :class="{'period__main--second': index % 2 === 0, 'green': index % 2 !== 0 }">
+                        <div class="period__content">
+                            <p class="period__title">{{index + 1}} Этап</p>
+                            <h2 class="period__title">{{period.title}}</h2>
+                            <p class="period__date">{{period.textDate}}</p>
+                            <p class="period__text">
+                                {{period.content}}
+                            </p>
+                            <a :href="period.registerLink"
+                               :class="{'button--second': index % 2 === 0}"
+                               class="button period__button">Регистрация</a>
+                        </div>
+                    </div>
+                    <g-image :src="image" class="period__image" v-for="image of period.images"
+                             :key="image"></g-image>
+                </article>
+            </div>
         </article>
+        <section class="news">
+            <div class="news__main">
+                <h2 class="news__title">Новости</h2>
+            </div>
+            <g-link class="news__item" >
+                <span class="news__item-bg" :style="{backgroundImage: 'url(' + $page.wordPressActivity.acf.banner + ')'}"></span>
+                <span class="news__item-content">
+                     2 и 3й этапы “Крымской осени” переносятся на июль
+                </span>
+            </g-link>
+            <g-link class="news__item">
+                <span class="news__item-bg" :style="{backgroundImage: 'url(' + $page.wordPressActivity.acf.banner + ')'}"></span>
+                <span class="news__item-content">
+                     2 и 3й этапы “Крымской осени” переносятся на июль
+                </span>
+            </g-link>
+            <g-link class="news__item">
+                <span class="news__item-bg" :style="{backgroundImage: 'url(' + $page.wordPressActivity.acf.banner + ')'}"></span>
+                <span class="news__item-content">
+                     2 и 3й этапы “Крымской осени” переносятся на июль
+                </span>
+            </g-link>
+            <g-link class="news__item">
+                <span class="news__item-bg" :style="{backgroundImage: 'url(' + $page.wordPressActivity.acf.banner + ')'}"></span>
+                <span class="news__item-content">
+                     2 и 3й этапы “Крымской осени” переносятся на июль
+                </span>
+            </g-link>
+        </section>
+        <section class="documents-section">
+            <div class="container documents-section__container">
+                <h2 class="documents-section__title">Документы</h2>
+                <div class="documents">
+                    <g-link class="documents__item">
+                    <Document class="documents__item-icon"/>
+                    <p  class="documents__item-text">Регламент гонки</p>
+                </g-link>
+                    <g-link class="documents__item">
+                        <Document class="documents__item-icon"/>
+                        <p  class="documents__item-text">Регламент гонки</p>
+                    </g-link>
+                    <g-link class="documents__item">
+                        <Document class="documents__item-icon"/>
+                        <p  class="documents__item-text">Регламент гонки</p>
+                    </g-link>
+                    <g-link class="documents__item">
+                        <Document class="documents__item-icon"/>
+                        <p  class="documents__item-text">Регламент гонки</p>
+                    </g-link>
+                    <g-link class="documents__item">
+                        <Document class="documents__item-icon"/>
+                        <p  class="documents__item-text">Регламент гонки</p>
+                    </g-link>
+                </div>
+            </div>
+        </section>
 
         {{$page}}
     </Layout>
@@ -155,28 +232,215 @@
     from
     to
     }
+    periods {
+    title
+    textDate
+    content
+    registerLink
+    video
+    images
+    }
     }
     }
     }
 </page-query>
 
 <script>
+    import Document from '../assets/svg/document.svg';
     export default {
+        components: {
+            Document
+        },
         metaInfo() {
             return {title: this.$page.wordPressActivity.title};
         },
         data: () => ({
-            stages: null
+            stages: null,
+            periods: []
         }),
         mounted() {
             if (this.$page) {
-                this.stages = Array(+this.$page.wordPressActivity.acf.numberOfStages).fill(1).map((_, i) => i+1)
+                this.stages = Array(+this.$page.wordPressActivity.acf.numberOfStages).fill(1).map((_, i) => i + 1)
             }
         }
     }
 </script>
 
 <style scoped lang="scss">
+
+    .documents-section {
+        background-color: var(--color-second-main);
+        padding: 85px 0;
+        color: var(--color-second);
+        background-image: url('../assets/images/design-element.png');
+        background-position: right bottom;
+        background-repeat: no-repeat;
+    }
+
+    .documents-section__title {
+        font-weight: 800;
+        font-size: 24px;
+        margin-bottom: 85px;
+    }
+
+    .documents {
+        display: grid;
+        grid-template-columns: repeat(6, 126px);
+        grid-gap: 20px;
+    }
+
+    .documents__item {
+        color: var(--color-second);
+        transition: all .3s ease-in-out;
+        &:hover {
+            color: var(--color-second-accent);
+            .documents__item-icon {
+                fill: var(--color-second-accent);
+            }
+        }
+    }
+
+    .documents__item-icon {
+        margin-bottom: 12px;
+        transition: all .3s ease-in-out;
+    }
+    .documents__item-text {
+        font-size: 18px;
+        line-height: 1.2;
+    }
+
+    .news {
+        color: #fff;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-auto-rows: 450px;
+    }
+
+    .news__main {
+        padding: 95px 20px 0;
+        display: flex;
+        text-align: center;
+        justify-content: center;
+        background: var(--color-second-accent);
+    }
+
+    .news__title {
+        text-transform: uppercase;
+        font-weight: 800;
+        font-size: 24px;
+    }
+
+    .news__item {
+        display: flex;
+        text-align: left;
+        background-size: cover;
+        font-size: 24px;
+        line-height: 31px;
+        padding: 95px 20px;
+        text-decoration: none;
+        position: relative;
+        transition: all .3s ease-in-out;
+        &:hover {
+            color: var(--color-second);
+            background: var(--color-main);
+            .news__item-bg {
+                filter: grayscale(.2);
+            }
+            .news__item-content {
+                color: var(--color-second);
+            }
+        }
+        &:hover {
+            outline: none;
+        }
+    }
+    .news__item-bg {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-position: center;
+        transition: all .3s ease-in-out;
+        opacity: .60;
+        filter: grayscale(1);
+    }
+
+    .news__item-content {
+        position: relative;
+        z-index: 1;
+        max-width: 50%;
+        text-decoration: none;
+        color: var(--color-main);
+        transition: all .3s ease-in-out;
+    }
+
+    .period {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-auto-rows: 450px;
+        color: var(--color-second);
+    }
+
+    .period__main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: var(--color-second-main);
+
+        &--second {
+            background-color: var(--color-second-accent);
+
+        }
+    }
+
+    .period__content {
+        width: 50%;
+    }
+
+    .period__title {
+        text-transform: uppercase;
+        font-weight: 800;
+        font-size: 24px;
+        line-height: 1.5;
+    }
+
+    .period__date {
+        font-size: 18px;
+        margin-bottom: 10px;
+    }
+
+    .period__text {
+        font-size: 12px;
+        line-height: 1.2;
+    }
+
+    .period__button {
+        margin-top: 20px;
+    }
+
+    .period__image {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+
+    .period__media {
+        position: relative;
+        overflow: hidden;
+        padding-top: 56.25%;
+    }
+
+    .period__media-iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border: 0;
+    }
+
     .activity-page {
         padding-top: 100px;
     }
@@ -392,6 +656,7 @@
     .timings__item-desc {
         font-size: 10px;
         line-height: 1.2;
+
         &:after {
             content: '';
             margin-top: 11px;
@@ -401,6 +666,7 @@
             background: var(--color-main);
         }
     }
+
     .timings__item-date {
         top: calc(100% + 5px);
         left: 0;
@@ -424,19 +690,22 @@
         text-transform: lowercase;
     }
 
-    .timings__item--end{
+    .timings__item--end {
         flex: inherit;
         width: 0;
+
         .timings__item-bot {
             border-left: none;
             border-right: 1px solid var(--color-main);
             padding-left: 0;
         }
+
         .timings__item-top {
             position: absolute;
             top: -33px;
             right: 0;
         }
+
         .timings__item-date {
             left: inherit;
             right: 0;
